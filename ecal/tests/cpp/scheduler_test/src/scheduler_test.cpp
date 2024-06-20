87fc58b15436496eb46f2fe2b10a27c52c33d64d
Peguen <73380451+Peguen@users.noreply.github.com>
2024-06-20 10:44:21 +0200

    Some adaptations.

--- ecal/tests/cpp/scheduler_test/src/scheduler_test.cpp
+++ ecal/tests/cpp/scheduler_test/src/scheduler_test.cpp
@@ -39,9 +39,8 @@
 
 struct InputA
 {
-  int gear;
+  int   gear;
   float speed;
-  std::vector<float> points;
 };
 
 struct OutputA
@@ -73,40 +72,34 @@ class InputOutputHandler
   public:
     InputOutputHandler() {
         // Initialize input subscribers
-      RareTs::Members<INPUT>::forEach(m_inputs, [&](auto member, auto& value) {
+      RareTs::Members<INPUT>::forEach(m_inputs, [&, this](auto member, auto& value) {
         m_input_subscriber.emplace_back(eCAL::CSubscriber(member.name));
-            m_input_subscriber.back().AddReceiveCallback([&value, &member](const char* , const struct eCAL::SReceiveCallbackData* data_){
-            std::cout << "Received something on inputSubscriber for membername " << member.name << "\n";
-
-                // Assign received data to the correct member
-                if constexpr (std::is_same_v<decltype(value), int&>) {
-                    value = std::stoi(std::string(static_cast<const char*>(data_->buf), data_->size));
-                } else if constexpr (std::is_same_v<decltype(value), float&>) {
-                    value = std::stof(std::string(static_cast<const char*>(data_->buf), data_->size));
-                } else if constexpr (std::is_same_v<decltype(value), std::vector<float>&>) {
-                    // Deserialize the data buffer into the vector<float>
-                    value.clear();
-                    std::istringstream iss(std::string(static_cast<const char*>(data_->buf), data_->size));
-                    std::copy(std::istream_iterator<float>(iss), std::istream_iterator<float>(), std::back_inserter(value));
-                } else {
-                    std::cerr << "Unsupported type for member " << member.name << "\n";
-                }
+            m_input_subscriber.back().AddReceiveCallback([&value](const char* , const struct eCAL::SReceiveCallbackData* data_){            
+                memcpy(&value, data_->buf, data_->size);                
             });
         });
 
         // Initialize output publishers
       RareTs::Members<OUTPUT>::forEach(m_outputs, [&](auto member, auto& value) {
           m_output_publisher.emplace_back(eCAL::CPublisher(member.name));
+          auto id = m_output_publisher.size() - 1;
+          m_send_functions.emplace_back([&, id](){
+            std::string buf;
+            buf.resize(sizeof(value));
+            memcpy(buf.data(), &value, sizeof(value));            
+            m_output_publisher[id].Send(buf);
+          });
         });
     }
 
     INPUT GetInputs() const { return m_inputs; }
+    OUTPUT& GetOutputs() { return m_outputs; }
     
     size_t SendOutputs() {       
       size_t all_send_bytes{0};
-      for(auto& pub : m_output_publisher)
+      for(auto& pub : m_send_functions)
       {
-        all_send_bytes = pub.Send("I send something to you!" + std::to_string(send_counter++));        
+        pub();        
       }
       return all_send_bytes;
     };
@@ -116,8 +109,9 @@ class InputOutputHandler
     OUTPUT m_outputs;
     unsigned int send_counter{0};
 
-    std::vector<eCAL::CSubscriber> m_input_subscriber;
-    std::vector<eCAL::CPublisher>  m_output_publisher;
+    std::vector<eCAL::CSubscriber>     m_input_subscriber;
+    std::vector<eCAL::CPublisher>      m_output_publisher;
+    std::vector<std::function<void()>> m_send_functions;
 };
 
 using AlgoRunMap = std::multimap<unsigned int, std::function<void()>>;
@@ -209,8 +203,8 @@ class Scheduler
     void RunLoop() {
         while (m_cycle_count < m_max_runs) {
         m_time.StartTimer();
-            for(auto& key_val : m_algo_map) {
-                if (m_cycle_count % key_val.first == 0) {
+        for(auto& key_val : m_algo_map) {
+          if (m_cycle_count % key_val.first == 0) {
             m_thread_pool.enqueue([&key_val] {
               key_val.second();
             });
@@ -225,33 +219,69 @@ class Scheduler
     }
     
     AlgoRunMap   m_algo_map;
-    TimeCapture  m_time{33U}; // 33ms cycle time
+    TimeCapture  m_time{1U};
     unsigned int m_cycle_count = 0;
     unsigned int m_max_runs = 1000;
     ThreadPool   m_thread_pool;
 
 };
 
+void InputAThreads()
+{
+  eCAL::CPublisher gear_pub("gear");
+  eCAL::CPublisher speed_pub("speed");
+  eCAL::CPublisher points_pub("points");
+
+  int counter = 0;
+  std::string gear_buf;
+  std::string speed_buf;
+  std::string points_buf;
+
+  gear_buf.resize(sizeof(counter));
+  speed_buf.resize(sizeof(float));
+
+  while (eCAL::Ok())
+  {    
+    memcpy(gear_buf.data(), &counter, sizeof(counter));
+    auto float_counter = static_cast<float>(counter%120);
+    memcpy(speed_buf.data(), &float_counter, sizeof(float_counter));
+
+    gear_pub.Send(gear_buf);
+    speed_pub.Send(speed_buf);
+    ++counter;
+    std::this_thread::sleep_for(std::chrono::microseconds(500));
+  }
+}
+
 TEST(core_cpp_scheduler, scheduler_test)
 {
   eCAL::Initialize();
-    Scheduler scheduler;
+  eCAL::Util::EnableLoopback(true);
+  
+  std::thread send_thread(&InputAThreads);
+  Scheduler scheduler;
 
-  InputOutputHandler<InputA, OutputA> myAlgo;
+  InputOutputHandler<InputA, OutputA> inputsOutputs;
   
     std::function<void()> run = [&] {
-        auto inputs = myAlgo.GetInputs();
+      static int counter = 0;
+        auto inputs = inputsOutputs.GetInputs();
         std::cout << "InputA gear: " << inputs.gear << "\n";
         std::cout << "InputA speed: " << inputs.speed << "\n";
-        for (const auto& point : inputs.points)
-      std::cout << "InputA points: " << point << "\n";
 
+        inputsOutputs.GetOutputs().message = std::to_string(counter++);
+        inputsOutputs.GetOutputs().message_size = sizeof(counter);
         // Fill outputs and send
-    myAlgo.SendOutputs();
+        // in InputOutputHandler send after execution
+        inputsOutputs.SendOutputs();
   };
 
-    scheduler.AddAlgo(33U, run);
-    scheduler.RunBoyRun();
+  scheduler.AddAlgo(33U, run);
+  scheduler.RunBoyRun();
 
+  eCAL::Finalize();
+
+  if (send_thread.joinable())
+    send_thread.join();
   std::cout << "Test ended." << std::endl;
 }
